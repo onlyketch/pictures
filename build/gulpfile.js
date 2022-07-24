c459d0dce5a09a8cb61943d88a0119b3945b48a0
Andrey <samojlenko.andrej@4px.ru>
2022-07-24 15:57:38 +0300

    Добавлена папка gulp сборки для верстки

---
+++ build/gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp'),
+	browserSync = require('browser-sync'),
+	inlineCss = require('gulp-inline-css'),
+	imagemin = require('gulp-imagemin'),
+	pngquant = require('imagemin-pngquant'),
+	mozJPEG = require('imagemin-mozjpeg'),
+	del = require('del');
+
+gulp.task('cleanImg', function() {
+	return del('dist/*.{jpg,png,gif}')
+});
+
+gulp.task('compress', function() {
+return gulp.src('source/*.{jpg,png}')
+	   .pipe(imagemin([
+	   			pngquant(),
+				mozJPEG({
+					quality: 95
+				})
+			], {
+					verbose: true
+			}))
+	   .pipe(gulp.dest('dist/'))
+	   .pipe(browserSync.reload({stream: true}))
+}); 
+
+gulp.task('inlineCss', function() {
+return gulp.src('source/*.html')
+ .pipe(inlineCss({
+ 	removeHtmlSelectors: true
+ }))
+ .pipe(gulp.dest('dist/'))
+ .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function() {
+	browserSync({ 
+		server: { 
+			baseDir: 'dist' 
+		},
+		notify: false
+	});
+});
+
+gulp.task('watch', function() {
+	gulp.watch('source/*.html', gulp.parallel('inlineCss'));
+	gulp.watch('source/*.{jpg,png}', gulp.parallel('cleanImg','compress'));
+});
+
+gulp.task('default', gulp.parallel('inlineCss', 'compress', 'browser-sync', 'watch'));
